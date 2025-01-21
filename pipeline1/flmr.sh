7a6fc50070f7ceb30a641705251637eda1193b4a
tcyuan373 <ty373@cornell.edu>
2025-01-21 17:55:53 -0500

    setup script update

--- pipeline1/flmr.sh
+++ pipeline1/flmr.sh
@@ -1,14 +1,17 @@
-conda create -n FLMR python=3.10 -y
+conda create -n FLMR python=3.12 -y
 conda activate FLMR
 
-
+#faiss gpu
+conda install -c pytorch/label/nightly -c nvidia faiss-gpu
+#torch and dependencies
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install ujson gitpython easydict ninja datasets transformers
 
+#FLMR
 git clone https://github.com/LinWeizheDragon/FLMR.git
 cd FLMR
 pip install -e .
-
+#ColBERT
 cd third_party/ColBERT
 pip install -e .
 
-pip install ujson gitpython easydict ninja datasets transformers
