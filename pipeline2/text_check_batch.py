3529877e12760b5d19efc85c314fa0ff91ca6d1f
az275 <az275@cornell.edu>
2025-03-25 23:48:39 +0000

    fix batch textcheck benchmark

--- pipeline2/text_check_batch.py
+++ pipeline2/text_check_batch.py
@@ -1,18 +1,15 @@
 from transformers import BartForSequenceClassification, BartTokenizer
 import torch
 import csv
+import pickle
 
 def textcheck(batch_premise, run_times):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-mnli')
-    model = BartForSequenceClassification.from_pretrained('facebook/bart-large-mnli').to(device)
-
     hypothesis = 'harmful.'
 
     # Tokenize all premises as a batch
     inputs = tokenizer(batch_premise, [hypothesis] * len(batch_premise), 
                        return_tensors='pt', padding=True, truncation=True).to(device)
-    
+
     model_start_event = torch.cuda.Event(enable_timing=True)
     model_end_event = torch.cuda.Event(enable_timing=True)
 
@@ -27,7 +24,7 @@ def textcheck(batch_premise, run_times):
     model_end_event.record()
     torch.cuda.synchronize()
     run_times.append((model_start_event.elapsed_time(model_end_event)) * 1e6)
-    
+
     logits = result.logits  # result[0] is now deprecated, use result.logits instead
 
     # Take only entailment and contradiction logits
@@ -35,19 +32,31 @@ def textcheck(batch_premise, run_times):
     probs = entail_contradiction_logits.softmax(dim=1)
     true_probs = probs[:, 1] * 100  # Probability of entailment
 
-    for i, prob in enumerate(true_probs.tolist()):
-        print(f'Premise {i+1}: Probability that the label is true: {prob:.2f}%')
+    # for i, prob in enumerate(true_probs.tolist()):
+    #     print(f'Premise {i+1}: Probability that the label is true: {prob:.2f}%')
 
     return true_probs.tolist()
 
 
 if __name__ == "__main__":
-    # Example batch
-    premise = 'A new model offers an explanation for how the Galilean satellites formed around the solar system’s largest world.'
-    batch_premise = [premise, premise, premise]
-    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-mnli')
+    model = BartForSequenceClassification.from_pretrained('facebook/bart-large-mnli').to(device)
+
+    file_path = '/mydata/msmarco/msmarco_3_clusters/doc_list.pkl'
+
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+
+    iterator = iter(data)
+
+    bsize = 8
     run_times = []
-    for i in range(100):
+
+    for i in range(1000):
+        batch_premise = []
+        for j in range(bsize):
+            batch_premise.append(next(iterator))
         textcheck(batch_premise, run_times)
 
     runtimes_file = 'text_check_batch_runtime.csv'
